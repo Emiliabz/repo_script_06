661259766e050af4cf2c7dac193657fccb6f0573
Emiliabz <emiliabrbz@gmail.com>
2026-01-29 17:30:49 -0300

    Add scripts

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# script_06.py
+
+Script extraÃ­do do arquivo original.
+
---
+++ script_06.py
@@ -0,0 +1,4 @@
+﻿n = int(input('Digite um nÃºmero: '))
+a = n-1
+b = n+1
+print('Analisando o valor de {}, seu antecessor Ã© {} e seu sucessor Ã© {}'.format(n,a,b))
